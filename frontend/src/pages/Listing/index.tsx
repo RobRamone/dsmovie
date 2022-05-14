511b21e177b2124bd28095eb3a64d48d559a68a4
RobRamone <robshacker@yahoo.com.br>
2022-05-14 11:55:54 -0300

    MovieCard

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,9 +1,33 @@
+import MovieCard from "componentes/MovieCard";
 import Pagination from "componentes/Pagination";
 
 function Listing() {
 
     return (
-        <Pagination />
+        <>
+            <Pagination />
+
+            <div className="container">
+                <div className="row">
+                    <div className="col-sms-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+                    <div className="col-sms-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+                    <div className="col-sms-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+                    <div className="col-sms-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+                    <div className="col-sms-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+                    
+                </div>
+            </div>
+        </>
 
     );
 }
